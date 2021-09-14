9a38b00e531077a53197aac400728e8ea6dba7a6
Jack-rm <jackcnsr94@gmail.com>
2021-09-14 15:22:45 +0200

    Added image according to vue's properties

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,9 @@ let app = new Vue({
         el: '#root',
         data: {
             message: 'questo framework mi piace tantissimo!',
-            classeTitolo: 'text-uppercase text-center fw-bold text-warning mt-3 mb-3',
+            mainTitle: 'text-uppercase fw-bold text-warning mt-3 mb-3',
+            roundedClass: 'rounded',
+            mainImg: 'https://miro.medium.com/max/6416/1*7OCwu--TWqVluPMsZdzWKw.png',
         }
     });
 
