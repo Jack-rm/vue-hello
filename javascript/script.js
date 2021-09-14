bc481e3e89024c3da2a7c38d3a049d253887b9dc
Jack-rm <jackcnsr94@gmail.com>
2021-09-14 15:05:35 +0200

    Initialization Vue framework exercise

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+        el: '#root',
+        data: {
+            message: 'questo framework mi piace tantissimo!',
+            classeTitolo: 'text-uppercase text-center fw-bold text-warning mt-3 mb-3',
+        }
+    });
+
